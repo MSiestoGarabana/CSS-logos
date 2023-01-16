f29609f130badb077d484dc64447023a20c4e4fc
MSiesto <miguelsiesto@gmail.com>
2023-01-16 12:57:16 +0100

    Create multi-colored Circle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div class="google-logo"></div>
+    </div>
+  );
 }
 
 export default App;
