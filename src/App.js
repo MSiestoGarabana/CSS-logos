d139157dc32bf675b93fb0e5263ebc5302fc9abc
MSiesto <miguelsiesto@gmail.com>
2023-02-26 13:27:22 +0100

    Importng windows logo and spacing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <div class="google-logo"></div>
+      <div className="microsoft-logo">
+        <div className="square red"></div>
+        <div className="square green"></div>
+        <div className="square blue"></div>
+        <div className="square yellow"></div>
+      </div>
     </div>
   );
 }
